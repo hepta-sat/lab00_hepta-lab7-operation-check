417358b17e716ccb5a43a4e87fc74c007177d641
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-07-24 01:30:53 +0000

    commit0724

---
+++ Grove_Air_Quality_Sensor_Library/Air_Quality.cpp
@@ -0,0 +1,114 @@
+//
+// modified by mbed group for use with the mbed platform
+// modification date 9/4/2014
+//
+
+/*
+  AirQuality library v1.0
+  2010 Copyright (c) Seeed Technology Inc.  All right reserved.
+
+  Original Author: Bruce.Qin
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include"Air_Quality.h"
+
+// Interrupt Handler Assignment
+Ticker IntHandler;
+
+//Get the avg voltage in 5 minutes.
+void AirQuality::avgVoltage()
+{
+    if(i==150) { //sum 5 minutes
+        vol_standard=temp/150;
+        temp=0;
+        printf("Vol_standard in 5 minutes: %d\n\r",vol_standard);
+        i=0;
+    } else {
+        temp+=first_vol;
+        i++;
+    }
+}
+
+void AirQuality::init(PinName pin, void(*IRQ)(void))
+{
+    _pin=pin;
+    AnalogIn sensor(_pin);
+    unsigned char i = 0;
+    printf("Air Quality Sensor Starting Up...(20s)\n\r");
+    wait(20); //20s
+    init_voltage = sensor.read() * 1000; // boost the value to be on a 0 -> 1000 scale for compatibility
+    printf("The initial voltage is %d%% of source \n\r",init_voltage/10);
+    while(init_voltage) {
+        if((init_voltage < 798) && (init_voltage > 10)) {
+            // the init voltage is ok
+            first_vol = sensor.read() * 1000;//initialize first value
+            last_vol = first_vol;
+            vol_standard = last_vol;
+            printf("Sensor ready.\n\r");
+            error = false;;
+            break;
+        } else if(init_voltage > 798 || init_voltage <= 10) {
+            // The sensor is not ready, wait a bit for it to cool off
+            i++;
+            printf("Sensor not ready (%d), try %d/5, waiting 60 seconds...\n\r",init_voltage,i);
+            wait(60);//60s
+            init_voltage = sensor.read() * 1000;
+            if(i==5) {
+                // After 5 minutes warn user that the sensor may be broken
+                i = 0;
+                error = true;
+                printf("Sensor Error! You may have a bad sensor. :-(\n\r");
+            }
+        } else
+            break;
+    }
+    // Call AirQualityInterrupt every 2seconds
+    IntHandler.attach(IRQ, 2.0);
+    printf("Test begin...\n\r");
+}
+
+int AirQuality::slope(void)
+{
+    while(timer_index) {
+        if(first_vol-last_vol > 400 || first_vol > 700) {
+            printf("High pollution! Force signal active.\n\r");
+            timer_index = 0;
+            avgVoltage();
+            return 0;
+        } else if((first_vol - last_vol > 400 && first_vol < 700) || first_vol - vol_standard > 150) {
+            printf("sensor_value:%d",first_vol);
+            printf("\t High pollution!\n\r");
+            timer_index = 0;
+            avgVoltage();
+            return 1;
+
+        } else if((first_vol-last_vol > 200 && first_vol < 700) || first_vol - vol_standard > 50) {
+            //printf(first_vol-last_vol);
+            printf("sensor_value:%d",first_vol);
+            printf("\t Low pollution!\n\r");
+            timer_index = 0;
+            avgVoltage();
+            return 2;
+        } else {
+            avgVoltage();
+            printf("sensor_value:%d",first_vol);
+            printf("\t Air fresh\n\r");
+            timer_index = 0;
+            return 3;
+        }
+    }
+    return -1;
+}
--- main.cpp
+++ main.cpp
@@ -1,12 +1,155 @@
 #include "mbed.h"
+#include "hcsr04.h" //超音波センサ
+#include "Dht11.h"  //
+#include"Air_Quality.h"
+#include "MPL3115A2.h"//大気圧
 
-DigitalOut myled(LED1);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+RawSerial pc(USBTX, USBRX); 
+HCSR04  usensor(p21,p12);//超音波センサ p21:trig p12:echo
+
+DigitalOut led0(LED1), led25(LED2), led50(LED3), led75(LED4);//光センサ
+AnalogIn temt6000(p18);//光センサ
+I2C i2c(p28, p27);       // sda, scl
+
+MPL3115A2 psensor(&i2c, &pc);//気圧
+ 
+DigitalOut myled(LED1);     // Sanity check to make sure the program is working.
+DigitalOut powerPin(p21);   // <-- I powered the sensor from a pin. You don't have to.
+AnalogIn sensorUV(p15);//紫外線
+ 
+//空気清浄度
+AirQuality airqualitysensor;
+int current_quality = -1;
+PinName analogPin = p15;
+// temt6000 breakout: VCC(to VOUT=3.3V), GND(to GND), SIG(to p15) 
+
+
+// Interrupt Handler
+void AirQualityInterrupt()
+{
+    AnalogIn sensor(analogPin);
+    airqualitysensor.last_vol = airqualitysensor.first_vol;
+    airqualitysensor.first_vol = sensor.read()*1000;
+    airqualitysensor.timer_index = 1;
 }
+Dht11 sensor(p15);//湿度
+//a:Uv
+//b:超音波
+//c:温湿度
+//d:光
+//e:air
+//f:気圧
+int main() {
+    char c;
+    int temp,humid;
+    float UVvalue;
+    int dist;
+    float x = temt6000;
+    
+    
+    while (true) {
+        pc.printf("-- enter any key --> ");
+        c = pc.getc();
+        if (c=='a'){//紫外線
+            for (int i=0;i<5;i++){
+                UVvalue = sensorUV;
+                printf("\rUV Value = %3.2f%%\r\n",UVvalue*100);
+                wait(0.5);
+            }
+        }
+        
+        else if(c=='b'){//超音波
+            for (int i=0;i<5;i++){
+                usensor.start();
+                dist=usensor.get_dist_cm();
+                pc.printf("distance = %d [cm]\r\n",dist);
+                wait(0.5);
+            }
+        }
+        
+        else if(c=='c'){//温・湿度
+            for (int i=0;i<5;i++){
+                sensor.read();
+                temp = sensor.getFahrenheit();
+                humid = sensor.getHumidity();
+                temp = (temp - 32.0)/1.80;    // Fahrenheit --> Celsius
+                pc.printf("T: %d, H: %d\r\n", temp, humid);
+                wait(0.5);
+            }
+        }
+
+        else if(c=='d'){//光
+            for (int i=0;i<5;i++){
+                // four LEDs meaning "light level" (1 to 4):
+                led0 = 1;
+                led25 = led50 = led75 = 0;
+                if(x>0.25) led25 = 1;
+                if(x>0.50) led50 = 1;
+                if(x>0.75) led75 = 1;
+                
+                printf("%f\r\n", x);
+                wait(0.50);
+            }
+        }
+        else if(c=='e'){//空気
+            airqualitysensor.init(analogPin, AirQualityInterrupt);
+            for (int i=0;i<5;i++){
+                current_quality=airqualitysensor.slope();
+                if (current_quality >= 0) { // if a valid data returned.
+                    if (current_quality == 0)
+                        printf("High pollution! Force signal active\n\r");
+                    else if (current_quality == 1)
+                        printf("High pollution!\n\r");
+                    else if (current_quality == 2)
+                        printf("Low pollution!\n\r");
+                    else if (current_quality == 3)
+                        printf("Fresh air\n\r");
+                }
+            }
+            
+        }else if(c=='f'){
+            
+            powerPin = 1;
+            wait_ms(300);
+
+            pc.printf("** MPL3115A2 SENSOR **\r\n");
+            psensor.init();
+
+            Altitude a;
+            Temperature t;
+            Pressure p;
+
+            // Offsets for Dacula, GA
+            psensor.setOffsetAltitude(83);
+            psensor.setOffsetTemperature(20);
+            psensor.setOffsetPressure(-32);
+                
+            for (int i=0;i<5;i++){
+                psensor.readAltitude(&a);
+                psensor.readTemperature(&t);
+                
+                psensor.setModeStandby();
+                psensor.setModeBarometer();
+                psensor.setModeActive();
+                psensor.readPressure(&p);
+                
+                pc.printf("Altitude: %sft, Temp: %sdegF, Pressure: %sPa\r\n", a.print(), t.print(), p.print());
+                
+                psensor.setModeStandby();
+                psensor.setModeAltimeter();
+                psensor.setModeActive();
+                
+                wait(1.0);
+            }
+        }
+        
+        
+    }
+    
+}
+
+
+
+
+
